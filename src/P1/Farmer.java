06edfcfdb5ff782647838dba8195f4a912eac785
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-09-06 15:04:42 +1000

    Added barebones farmer class

---
+++ src/P1/Farmer.java
@@ -0,0 +1,11 @@
+public class Farmer {
+    String farmerID;
+
+    public Farmer(String _farmerID){
+        farmerID = _farmerID;
+    }
+
+    public String getFarmerID(){
+        return farmerID;
+    }
+}
