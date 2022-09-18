768bd2aa837cba209b2a84b8eb3d2db1f6276925
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-09-18 20:30:02 +1000

    Added runnable method

--- src/P1/Farmer.java
+++ src/P1/Farmer.java
@@ -1,11 +1,69 @@
-public class Farmer {
-    String farmerID;
+public class Farmer implements Runnable{
+    private String farmerID;
+    private String direction;
+    private Bridge bridge;
+    private int numSteps;
+    
 
-    public Farmer(String _farmerID){
+    public Farmer(String _farmerID, Bridge _bridge, String _direction){
         farmerID = _farmerID;
+        bridge = _bridge;
+        direction = _direction;
+        numSteps = 0;
     }
 
     public String getFarmerID(){
         return farmerID;
+    }
+
+    public String getFarmerDirection(){
+        return direction;
+    }
+
+    public void changeFarmerDirection(){
+        if(direction.equals("North")){
+            direction = "South";
+        }
+        else{
+            direction = "North";
+        }
+    }
+
+    @Override
+    public void run() {
+        while(bridge.getNEON() < 10){
+            
+            try{
+                System.out.println(farmerID + ": Waiting for bridge. Going towards " + direction);
+                bridge.getBlock().acquire();
+            }
+            catch (Exception e) {
+                break;
+            }
+
+            if(bridge.getNEON() >= 10){
+                bridge.getBlock().release();
+                return;
+            }
+
+            while(numSteps != 20){
+                numSteps += 5;
+                System.out.println(farmerID + ": Crossing bridge step " + numSteps + ".");
+                try{
+                    Thread.sleep(20);
+                }
+                catch (Exception e) {
+                    break;
+                }
+            }
+    
+            numSteps = 0;
+            bridge.incrementNEON();
+            changeFarmerDirection();
+            System.out.println(farmerID + ": Across the bridge.");
+            System.out.println("NEON = " + bridge.getNEON() + "");
+            bridge.getBlock().release();
+            
+        }
     }
 }
