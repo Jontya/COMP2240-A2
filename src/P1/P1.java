971bb1cc5ca457b8c3c3649e2625f3656370fbdb
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-09-18 20:29:48 +1000

    Updated file reading method

--- src/P1/P1.java
+++ src/P1/P1.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class P1{
     private static String filename;
-    private ArrayList<Farmer> northFarmers;
-    private ArrayList<Farmer> southFarmers;
     public static void main(String[] args) throws Exception{
         try{
             filename = args[0];
@@ -18,44 +16,42 @@ public class P1{
     }
 
     private void run() throws Exception{
-        readFile();
-        for(int i = 0; i < northFarmers.size(); i++){
-            System.out.println(northFarmers.get(i).getFarmerID() + " " + southFarmers.get(i).getFarmerID());
+        ArrayList<Farmer> farmers = readFile();
+        ArrayList<Thread> threads = new ArrayList<>();
+        for(int i = 0; i < farmers.size(); i++){
+            threads.add(new Thread(farmers.get(i)));
+        }
+        
+        for(int i = 0; i < threads.size(); i++){
+            threads.get(i).start();
         }
     }
 
-    private void readFile() throws Exception{
+    private ArrayList<Farmer> readFile() throws Exception{
         File file = new File(filename);
         if(!file.exists()){
             System.out.println("File Not Found");
             System.exit(0);
         }
 
-        northFarmers = new ArrayList<>();
-        southFarmers = new ArrayList<>();
+        ArrayList<Farmer> farmer = new ArrayList<>();
+        Bridge bridge = new Bridge();
         Scanner scanner = new Scanner(file);
 
         try{
             String[] input = scanner.nextLine().split("[,]", 0);
-            if(input[0].charAt(0) == 'N'){
-                initFarmerLists(input[0], input[1]);
+            for(int i = 0; i < Character.getNumericValue(input[0].charAt(input[0].length() - 1)); i++){
+                farmer.add(new Farmer("N_Farmer" + (i + 1), bridge, "South"));
             }
-            else{
-                initFarmerLists(input[1], input[0]);
+
+            for(int i = 0; i < Character.getNumericValue(input[1].charAt(input[1].length() - 1)); i++){
+                farmer.add(new Farmer("S_Farmer" + (i + 1), bridge, "North"));
             }
         } 
         finally{
             scanner.close();
         }
-    }
 
-    private void initFarmerLists(String north, String south){
-        for(int i = 0; i < Character.getNumericValue(north.charAt(north.length()-1)); i++){
-            northFarmers.add(new Farmer("N_Farmer" + (i+1)));
-        }
-
-        for(int i = 0; i < Character.getNumericValue(south.charAt(south.length()-1)); i++){
-            southFarmers.add(new Farmer("S_Farmer" + (i+1)));
-        }
+        return farmer;
     }
 } 
