d8efeaa8578a7401cac5a933d2cdbf866f1474f9
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-09-20 00:04:49 +1000

    Removed unused methods

--- src/P1/Bridge.java
+++ src/P1/Bridge.java
@@ -2,25 +2,15 @@ import java.util.concurrent.*;
 
 public class Bridge{
     private Semaphore block;
-    private boolean running;
     private int NEON;
 
     public Bridge(){
         block = new Semaphore(1, true);
-        running = true;
         NEON = 0;
     }
 
     public Semaphore getBlock(){
         return block;
-    }
-
-    public boolean getRunning(){
-        return running;
-    }
-
-    public void setRunning(boolean _running){
-        running = _running;
     }
 
     public int getNEON(){
