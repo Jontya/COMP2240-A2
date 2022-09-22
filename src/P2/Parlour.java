12aa226c1bba74da6e190ee9a8759783c1d8eebf
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-09-22 18:26:10 +1000

    Updated semaphore handling

--- src/P2/Parlour.java
+++ src/P2/Parlour.java
@@ -1,96 +1,148 @@
-import java.util.concurrent.*;
 import java.util.ArrayList;
-import java.lang.Math;
 import java.util.PriorityQueue;
+import java.util.concurrent.*;
 
 public class Parlour {
-    private PriorityQueue<TimeEvent> eventQueue;
     private static final int MAXSEATS = 5;
-    private Semaphore timeSemaphore;
-    private Semaphore semaphore;
-    private boolean complete;
+    private ArrayList<Customer> finishedList;
+    private PriorityQueue<Integer> eventQueue;
+    private Semaphore mutex;
+    private Semaphore block;
+    private int customersEating;
+    private boolean capacityStatus;
     private int currTime;
 
-    private int checkCount;
 
     public Parlour(){
-        currTime = 0;
-        complete = false;
-        timeSemaphore = new Semaphore(1, true);
-        semaphore = new Semaphore(MAXSEATS, true);
         eventQueue = new PriorityQueue<>();
-
-        checkCount = 0;
+        finishedList = new ArrayList<>();
+        mutex = new Semaphore(1, true);
+        block = new Semaphore(MAXSEATS, true);
+        customersEating = 0;
+        currTime = 0;
+        capacityStatus = false;
     }
 
     public int getCurrTime(){
         return currTime;
     }
 
-    public void setCurrTime(int _currTime){
-        currTime = _currTime;
+    public boolean getCapacityStatus(){
+        return capacityStatus;
     }
 
-    public Semaphore getSemaphore(){
-        return semaphore;
-    }
-
-    public Semaphore getTimeSemaphore(){
-        return timeSemaphore;
-    }
-
-    public boolean getCompletedStatus(){
-        return complete;
-    }
-
-    public void swapCompletedStatus(){
-        complete = !complete;
-    }
-
-    public void incCheckCount(){
-        checkCount++;
-    }
-    
-    public boolean allChecked(){
-        if(checkCount == Math.abs(semaphore.availablePermits() - MAXSEATS)){
-            return true;
+    public void acquireBlock(){
+        try{
+            block.acquire();
         }
-        return false;
-    }
-
-    public void resetCheckCount(){
-        checkCount = 0;
-    }
-
-    public void nextEvent(){
-        if(!eventQueue.isEmpty()){
-            currTime = eventQueue.remove().getTime();
+        catch (Exception e){
             return;
         }
-        System.exit(0);
     }
+
+    public void acquireMutex(){
+        try{
+            mutex.acquire();
+        }
+        catch (Exception e){
+            return;
+        }
+    }
+
+    public void releaseBlock(){
+        block.release();
+    }
+
+    public void releaseMutex(){
+        mutex.release();
+    }
+
+    public boolean sitDown(){
+        if(!capacityStatus){
+            customersEating++;
+
+            if(customersEating == MAXSEATS){
+                capacityStatus = true;
+            }
+
+            acquireBlock();
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean leave(){
+        customersEating--;
+
+        if(customersEating == 0 && capacityStatus){
+            capacityStatus = false;
+        }
+
+        block.release();
+        return true;
+    }
+
+    public void addNewEvent(int time){
+        eventQueue.add(time);
+    }
+
+    public void insertIntoList(){
+        if(!eventQueue.isEmpty()){
+            currTime = eventQueue.remove();
+            eventQueue.add(eventQueue.peek());
+        }
+    }
+
+    public void incTime(){
+        if(!eventQueue.isEmpty()){
+            currTime = eventQueue.remove();
+        }
+    }
+
+    public void addToFinishedList(Customer customer){
+        finishedList.add(customer);
+    }
+
 
     public void runParlour(ArrayList<Customer> customers){
-        for(int i = 0; i < 8; i++){
-            new Thread(customers.get(i)).start();
-        }
-        System.out.println(semaphore.availablePermits());
-        /* 
-        Customer tempCustomer;
+        Customer temp;
         int size = customers.size();
-        for(int i = 0 ; i < size; i++){
-            tempCustomer = customers.get(i);
-            if(customers.get(i).getArriveTime() == 0){
-                tempCustomer.setSeatedTime(0);
-                eventQueue.add(new TimeEvent(tempCustomer, true, tempCustomer.getEatingTime()));
-                new Thread(customers.get(i)).start();
+        ArrayList<Thread> threads = new ArrayList<>();
+        for(int i = 0; i < size; i++){
+            temp = customers.remove(0);
+            if(temp.getArriveTime() == 0){
+                eventQueue.add(temp.getEatingTime());
             }
             else{
-                //eventQueue.add(new TimeEvent(tempCustomer, false, tempCustomer.getArriveTime()));
+                eventQueue.add(temp.getArriveTime());
             }
+            threads.add(new Thread(temp));
         }
-        System.out.println(semaphore.availablePermits());
-        //nextEvent();*/
+
+        for(int i = 0; i < threads.size(); i++){
+            threads.get(i).start();
+        }
+
+        incTime();
+        while(finishedList.size() != size);
+        System.out.println(generateReport());
     }
 
+    public String generateReport(){
+        String output = "Customer   Arrives     Seats    Leaves\n";
+        int pos = 1;
+        while(!finishedList.isEmpty()){
+            for(int i = 0; i < finishedList.size(); i++){
+                Customer temp = finishedList.get(i);
+                if(Character.getNumericValue(temp.getCustomerID().charAt(temp.getCustomerID().length() - 1)) == pos){
+                    output += String.format("%-11s %-10d %-8d %-1d", temp.getCustomerID(), temp.getArriveTime(), temp.getSeatedTime(), temp.getLeaveTime()) + "\n";
+                    finishedList.remove(i);
+                }
+            }
+            pos++;
+        }
+        return output;
+    }
 }
